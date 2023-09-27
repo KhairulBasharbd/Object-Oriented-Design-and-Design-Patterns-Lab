71eb0ae0cde4f9b626fad516d501766892b81ac0
Khairul Basar <khairulbashar24853@gmail.com>
2023-09-27 15:01:57 +0600

    Multiple Inheritance in java is done

---
+++ Main.java
@@ -0,0 +1,47 @@
+
+
+interface Vehicle{
+    void start();
+    //void stop();
+}
+
+interface Aircraft{
+    void takeOff();
+    //void landOn();
+}
+
+class FlyingCar implements Vehicle,Aircraft{
+
+    private String name;
+    private String model;
+
+    FlyingCar(String name, String model){
+        this.name=name;
+        this.model=model;
+
+    }
+
+    public void start(){
+        System.out.println(name+" "+model+" flying car is starting");
+    }
+
+    public void takeOff(){
+
+        System.out.println(name+" "+model+" flying car is taking off");
+
+    }
+
+
+}
+
+
+
+public class Main {
+    public static void main(String[] args){
+
+        FlyingCar flyingCar = new FlyingCar("Runner", "X99");
+
+        flyingCar.start();
+        flyingCar.takeOff();
+    }
+}
