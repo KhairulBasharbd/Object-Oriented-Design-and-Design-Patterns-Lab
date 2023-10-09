be543676a8fbcdbc65a3816b148ad9232aa5b2ac
Khairul Basar <khairulbashar24853@gmail.com>
2023-10-09 16:48:20 +0600

    Assignment 3 added

--- Assignment-3- Implementation of a facade design pattern for a Banking Service/Client.java
+++ Assignment-3- Implementation of a facade design pattern for a Banking Service/Client.java
@@ -1,27 +1,16 @@
-// CheckingAccount.java
 
-
-// SavingsAccount.java
-
-// InvestmentAccount.java
-
-
-// BankingServiceFacade.java
-
-// Main.java
-public class Main {
+public class Client {
     public static void main(String[] args) {
         BankingServiceFacade bankingService = new BankingServiceFacade();
 
-        // Deposit and withdraw from Checking Account
+        
         bankingService.depositToChecking(1000);
         bankingService.withdrawFromChecking(500);
 
-        // Deposit and withdraw from Savings Account
+        
         bankingService.depositToSavings(2000);
         bankingService.withdrawFromSavings(1000);
 
-        // Invest and withdraw from Investment Account
         bankingService.investInInvestmentAccount(5000);
         bankingService.withdrawFromInvestmentAccount(2000);
     }
